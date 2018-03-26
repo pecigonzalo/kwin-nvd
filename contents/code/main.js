a5b5462712ade821d67d3b475ed1220715f03300
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2018-03-26 23:03:10 +0200

    Refactor and add support for minimize and unminimize

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,53 +1,141 @@
-var nvd = {
-  state: {
-    savedDesktops: {},
-    enabled: true
-  },
-  log: function(msg) {
-    print("NVD: " + msg);
-  },
-  moveToNewDesktop: function(client) {
-    this.state.savedDesktops[client.windowId] = client.desktop;
+var state = {
+  savedDesktops: {}
+};
 
-    var next = workspace.desktops + 1;
-    workspace.desktops = next;
-    client.desktop = next;
-    workspace.currentDesktop = next;
-    workspace.activateClient = client;
-  },
-  moveBack: function(client) {
-    var saved = this.state.savedDesktops[client.windowId];
-    if (saved === undefined) {
-      this.log("Ignoring window not previously seen: " + client.caption);
-    } else {
-      this.log("Resotre client desktop to " + saved);
-      client.desktop = saved;
-      workspace.currentDesktop = saved;
-      workspace.activateClient = client;
+function log(msg) {
+  print("NVD: " + msg);
+}
 
-      workspace.desktops -= 1;
+function createDesktop() {
+  log("createDesktop: true");
+  var next = workspace.desktops + 1;
+  workspace.desktops = next;
+  return next;
+}
+
+function clientsInDesktop(desktop) {
+  return workspace.clientList().filter(
+    function(client) {
+      if (client.desktop == desktop) {
+        log("clientsInDesktop " + desktop + ": " + client.caption);
+        return client.desktop;
+      }
     }
-  },
-  fullHandler: function(client, full, user) {
-    if (full) {
-      nvd.moveToNewDesktop(client);
-    } else {
-      nvd.moveBack(client);
+  );
+}
+
+function clientsAfterDesktop(desktop) {
+  return workspace.clientList().filter(
+    function(client) {
+      return client.desktop > desktop;
     }
-  },
-  install: function() {
-    workspace.clientMaximizeSet.connect(this.fullHandler);
-    // registerShortcut(
-    //   "Maximize to new VirtualDesktop",
-    //   "Maximize to new VirtualDesktop",
-    //   "Shift+F12",
-    //   function() {
-    //     fullHandler();
-    //   }
-    // );
-    this.log("Shortcut installed");
+  );
+}
+
+function pullClientsAfterDesktop(desktop) {
+  clientsAfter = clientsAfterDesktop(desktop);
+  if (clientsAfter.length > 0) {
+    clientsAfter.forEach(
+      function(c) {
+        c.desktop--;
+      }
+    );
   }
 }
 
-nvd.log("Installing shortcut");
-nvd.install();
+function pushClientsAfterDesktop(desktop) {
+  clientsAfter = clientsAfterDesktop(desktop);
+  log("pushClientsAfterDesktop: count: " + clientsAfter.length);
+  if (clientsAfter.length > 0) {
+    clientsAfter.forEach(
+      function(c) {
+        log("pushClientsAfterDesktop: " + c.caption);
+        if (c.desktop == workspace.desktops) {
+          createDesktop();
+        }
+        c.desktop++;
+      }
+    );
+  }
+}
+
+function moveToNewDesktop(client) {
+  // Only maximize to new desktop is current desktop is full
+  if (clientsInDesktop(client.desktop).length > 1) {
+    log("Curent desktop has clients, moving to new desktop");
+    // Save window state
+    state.savedDesktops[client.windowId] = client.desktop;
+    if (client.desktop == workspace.desktops) {
+      createDesktop();
+    } else {
+      pushClientsAfterDesktop(client.desktop);
+    }
+    client.desktop++;
+    workspace.currentDesktop++;
+    workspace.activateClient = client;
+  }
+}
+
+function moveBack(client) {
+  var old = client.desktop;
+  var saved = this.state.savedDesktops[client.windowId];
+  if (saved === undefined) {
+    log("Ignoring window not previously seen: " + client.caption);
+  } else {
+    log("Resotre client desktop to " + saved);
+    client.desktop = saved;
+    workspace.currentDesktop = saved;
+    workspace.activateClient = client;
+    if (clientsInDesktop(old).length == 0) {
+      log("moveBack: will reduce max desktops");
+      pullClientsAfterDesktop(old);
+      workspace.desktops--;
+    }
+  }
+}
+
+function clientMaximizeHandle(client, h, v) {
+  log("Maximize Handle: " + client.caption + " - " + client.desktop);
+
+  // If desktop is -1, this window is on all desktops and there is nothig to do
+  if (client.desktop != -1) {
+    if (h && v) {
+      log("Maximize Handle: maximize");
+      moveToNewDesktop(client);
+    } else {
+      log("Maximize Handle: unmaximize");
+      moveBack(client);
+    }
+  }
+}
+
+function clientUnminimizedHandler(client) {
+  log("Unminimize Handle: " + client.caption + " - " + client.desktop);
+
+  // If desktop is -1, this window is on all desktops and there is nothig to do
+  if (client.desktop != -1) {
+    moveToNewDesktop(client);
+  }
+}
+
+function clientCloseHandler(client) {
+  log("Close Handle: " + client.caption + " - " + client.desktop);
+
+  // If desktop is -1, this window is on all desktops and there is nothig to do
+  if (client.desktop != -1) {
+    if (clientsInDesktop(client.desktop).length == 1) {
+      log("clientCloseHandler: will reduce max desktops");
+      pullClientsAfterDesktop(client.desktop);
+      workspace.currentDesktop--;
+      client.desktop--;
+      workspace.desktops--;
+    }
+  }
+}
+
+log("Installing shortcuts");
+workspace.clientMaximizeSet.connect(clientMaximizeHandle);
+workspace.clientUnminimized.connect(clientUnminimizedHandler);
+workspace.clientMinimized.connect(clientCloseHandler);
+workspace.clientRemoved.connect(clientCloseHandler);
+log("Shortcuts installed");
